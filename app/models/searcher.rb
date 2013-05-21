fde4abb2d674c116fb894ddbc4dd7b309243fe07
Kate Daly <alexdaly@Alexs-MacBook-Pro.local>
2013-05-21 13:35:53 -0400

    class attribute in searcher

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -1,11 +1,14 @@
 class Searcher
 
+class_attribute :backend #allows to set it on class and instance level
+self.backend = Twitter
+
 def initialize(term)
   @term = term
 end
 
 def each(&block)
-  Twitter.search(@term).results.each(&block)
+  backend.search(@term).results.each(&block)
 end
 
 end
