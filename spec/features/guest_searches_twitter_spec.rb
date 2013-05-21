1d4d0e2807e63546ebc007643e71413daeb7e5b3
Kate Daly <alexdaly@Alexs-MacBook-Pro.local>
2013-05-21 15:04:36 -0400

    refactoring tests

--- spec/features/guest_searches_twitter_spec.rb
+++ spec/features/guest_searches_twitter_spec.rb
@@ -5,38 +5,31 @@ require 'spec_helper'
 feature 'Guest searches for a hashtag' do
   scenario 'displays search results' do
     search_for '#rails'
-
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: /#rails/i, count: 15
   end
 
   scenario 'dislays known information' do
-    Searcher.backend = FakeTwitter
-    FakeTwitter['#rails'] = 5.times.map do
-      { text: 'I love #rails'}
-    end
-
+    search_fake_twitter '#rails', 5.times.map {{ text: 'I love #rails'}}
     search_for '#rails'
-
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: 'I love #rails', count: 5
   end
 
   scenario 'searches without a hashtag' do
+    search_fake_twitter '#rails', 5.times.map {{ text: 'I love #rails'}}
     search_for 'rails'
-
     expect(current_path).to eq '/searches/rails'
-    expect(page).to have_css 'li', text: /#rails/i, count: 15
+    expect(page).to have_css 'li', text: /#rails/i, count: 5
   end
 
   scenario 'updates search results' do
+    search_fake_twitter '#ruby', 5.times.map {{ text: 'I love #ruby'}}
     search_for '#rails'
-
     fill_in 'Search', with: '#ruby'
     click_on 'Submit'
-
     expect(current_path).to eq '/searches/ruby'
-    expect(page).to have_css 'li', text: /#ruby/i, count: 15
+    expect(page).to have_css 'li', text: /#ruby/i, count: 5
   end
 
   def search_for(term)
@@ -45,6 +38,10 @@ feature 'Guest searches for a hashtag' do
     click_on 'Submit'
   end
 
+  def search_fake_twitter(term, results)
+    Searcher.backend = FakeTwitter
+    FakeTwitter[term] = results
+  end
 
 
 
