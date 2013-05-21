acfae2d53d33e8778295c2dc67d1be3fe1b2e1a9
Kate Daly <alexdaly@Alexs-MacBook-Pro.local>
2013-05-21 12:17:13 -0400

    basic twitter search

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,15 @@
+class SearchesController < ApplicationController
+
+def new
+end
+
+def create
+  search_term = params[:search][:search] #1st form, 2nd field data as string (including pound sign, see below for a fix)
+  redirect_to search_path(search_term.gsub(/#/, ''))
+end
+
+def show
+  @tweets = Twitter.search("##{params[:id]}").results #:id because it's show action, two pound signs becasue we need the hash, but removed it previously
+end
+
+end
